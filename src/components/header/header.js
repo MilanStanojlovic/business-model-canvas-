d4d921521d2872b466deda3fb08090b87ca326ea
Milan <mistikone1@gmail.com>
2020-08-14 15:02:44 +0200

    header base styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import style from './header.module.scss';
+import grid from '../../styles/global/grid.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudDownloadAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
     <header className={style.header}>
-      Header
+      <div className={grid.grid}>
+        <span className={style.header__logo}>
+          Logo
+        </span>
+        <nav className={style.navigation}>
+          <ul>
+            <li><FontAwesomeIcon icon={faCloudDownloadAlt} size="lg" /></li>
+            <li><FontAwesomeIcon icon={faUser} size="lg" /></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
