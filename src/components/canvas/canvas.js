49222143394283712a8463a81faec26de9048f0a
Milan <mistikone1@gmail.com>
2020-08-15 12:27:35 +0200

    canvas and list

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,36 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './canvas.module.scss';
 import List from '../list/list';
+import { canvasList } from '../../enum/canvasList';
 
 const Canvas = () => {
+  const [partners, setPartners] = useState([]);
+  const [activities, setActivities] = useState([]);
+  const [resources, setResources] = useState([]);
+  const [values, setValues] = useState([]);
+  const [customerRelationships, setCustomerRelationships] = useState([]);
+  const [chanels, setChanels] = useState([]);
+  const [customerSegments, setCustomerSegments] = useState([]);
+  const [costStructure, setCostStructure] = useState([]);
+  const [revenueStreams, setRevenueStreams] = useState([]);
+
+  const addElementHandler = (listName) => {
+    switch (listName) {
+      case canvasList.KEY_PARTNERS: console.log('Partners');
+        break;
+      case canvasList.KEY_ACTIVITIES: console.log('Activities');
+        break;
+      case canvasList.KEY_RESOURCES: console.log('RESOURCES');
+        break;
+      case canvasList.VALUE_PROPOSITION: console.log('VALUE PROPOSITION');
+        break;
+      case canvasList.CUSTOMER_RELATIONSHIPS: console.log('Customer Relationships');
+        break;
+      case canvasList.CHANELS: console.log('Chanels');
+        break;
+      case canvasList.CUSTOMER_SEGMENTS: console.log('Customer Segments');
+        break;
+      case canvasList.COST_STRUCTURE: console.log('Const Structure');
+        break;
+      case canvasList.REVENUE_STREAMS: console.log('Revenue streams');
+        break;
+      default: throw new ReferenceError('Unknown type.');
+    }
+  }
+
   return (
     <section className={style.canvas}>
       <div className={style.canvas__partners}>
-        <List name="Key Partners" />
+        <List name={canvasList.KEY_PARTNERS} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__activities}>
-        <List name="Key Activities" />
+        <List name={canvasList.KEY_ACTIVITIES} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__resources}>
-        <List name="Key Resources" />
+        <List name={canvasList.KEY_RESOURCES} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__value}>
-        <List name="Value Proposition" />
+        <List name={canvasList.VALUE_PROPOSITION} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__customerRelationship}>
-        <List name="Customer Relatinships" />
+        <List name={canvasList.CUSTOMER_RELATIONSHIPS} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__chanels}>
-        <List name="Chanels" />
+        <List name={canvasList.CHANELS} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__customerSegments}>
-        <List name="Customer Segments" />
+        <List name={canvasList.CUSTOMER_SEGMENTS} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__cost}>
-        <List name="Cost structure" />
+        <List name={canvasList.COST_STRUCTURE} addCard={addElementHandler} />
       </div>
       <div className={style.canvas__revenue}>
-        <List name="Revenue Streams" />
+        <List name={canvasList.REVENUE_STREAMS} addCard={addElementHandler} />
       </div>
     </section>
   )
