c17183bd3a123ddd326ae5e299e2a41e499240aa
Milan <mistikone1@gmail.com>
2020-08-15 12:26:55 +0200

    canvas list

---
+++ src/enum/canvasList.js
@@ -0,0 +1,11 @@
+export const canvasList = {
+  KEY_PARTNERS: 'Key Partners',
+  KEY_ACTIVITIES: 'Key Activities',
+  KEY_RESOURCES: 'Key Resources',
+  VALUE_PROPOSITION: 'Value Proposition',
+  CUSTOMER_RELATIONSHIPS: 'Customer Relatinships',
+  CHANELS: 'Chanels',
+  CUSTOMER_SEGMENTS: 'Customer Segments',
+  COST_STRUCTURE: 'Cost structure',
+  REVENUE_STREAMS: 'Revenue Streams'
+}
