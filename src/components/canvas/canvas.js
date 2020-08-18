b393b489f25ca6458893248f59afe23ff6725103
Milan <mistikone1@gmail.com>
2020-08-18 13:31:21 +0200

    modal integration

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import style from './canvas.module.scss';
 import List from '../list/list';
 import { canvasList } from '../../enum/canvasList';
+import Modal from '../modal/modal';
 
 const Canvas = () => {
   const [partners, setPartners] = useState([]);
@@ -14,60 +15,70 @@ const Canvas = () => {
   const [costStructure, setCostStructure] = useState([]);
   const [revenueStreams, setRevenueStreams] = useState([]);
 
+  const [selectedList, setSelectedList] = useState(undefined);
+
+
   const addElementHandler = (listName) => {
     switch (listName) {
-      case canvasList.KEY_PARTNERS: console.log('Partners');
+      case canvasList.KEY_PARTNERS: setSelectedList(canvasList.KEY_PARTNERS);
         break;
-      case canvasList.KEY_ACTIVITIES: console.log('Activities');
+      case canvasList.KEY_ACTIVITIES: setSelectedList(canvasList.KEY_ACTIVITIES);
         break;
-      case canvasList.KEY_RESOURCES: console.log('RESOURCES');
+      case canvasList.KEY_RESOURCES: setSelectedList(canvasList.KEY_RESOURCES);
         break;
-      case canvasList.VALUE_PROPOSITION: console.log('VALUE PROPOSITION');
+      case canvasList.VALUE_PROPOSITION: setSelectedList(canvasList.VALUE_PROPOSITION)
         break;
-      case canvasList.CUSTOMER_RELATIONSHIPS: console.log('Customer Relationships');
+      case canvasList.CUSTOMER_RELATIONSHIPS: setSelectedList(canvasList.CUSTOMER_RELATIONSHIPS);
         break;
-      case canvasList.CHANELS: console.log('Chanels');
+      case canvasList.CHANELS: setSelectedList(canvasList.CHANELS)
         break;
-      case canvasList.CUSTOMER_SEGMENTS: console.log('Customer Segments');
+      case canvasList.CUSTOMER_SEGMENTS: setSelectedList(canvasList.CUSTOMER_SEGMENTS);
         break;
-      case canvasList.COST_STRUCTURE: console.log('Const Structure');
+      case canvasList.COST_STRUCTURE: setSelectedList(canvasList.COST_STRUCTURE);
         break;
-      case canvasList.REVENUE_STREAMS: console.log('Revenue streams');
+      case canvasList.REVENUE_STREAMS: setSelectedList(canvasList.REVENUE_STREAMS);
         break;
-      default: throw new ReferenceError('Unknown type.');
+      default: setSelectedList(undefined);
     }
   }
 
+  const handleModalClose = () => {
+    setSelectedList(undefined);
+  }
+
   return (
-    <section className={style.canvas}>
-      <div className={style.canvas__partners}>
-        <List name={canvasList.KEY_PARTNERS} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__activities}>
-        <List name={canvasList.KEY_ACTIVITIES} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__resources}>
-        <List name={canvasList.KEY_RESOURCES} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__value}>
-        <List name={canvasList.VALUE_PROPOSITION} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__customerRelationship}>
-        <List name={canvasList.CUSTOMER_RELATIONSHIPS} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__chanels}>
-        <List name={canvasList.CHANELS} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__customerSegments}>
-        <List name={canvasList.CUSTOMER_SEGMENTS} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__cost}>
-        <List name={canvasList.COST_STRUCTURE} addCard={addElementHandler} />
-      </div>
-      <div className={style.canvas__revenue}>
-        <List name={canvasList.REVENUE_STREAMS} addCard={addElementHandler} />
-      </div>
-    </section>
+    <Fragment>
+      <section className={style.canvas}>
+        <div className={style.canvas__partners}>
+          <List name={canvasList.KEY_PARTNERS} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__activities}>
+          <List name={canvasList.KEY_ACTIVITIES} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__resources}>
+          <List name={canvasList.KEY_RESOURCES} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__value}>
+          <List name={canvasList.VALUE_PROPOSITION} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__customerRelationship}>
+          <List name={canvasList.CUSTOMER_RELATIONSHIPS} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__chanels}>
+          <List name={canvasList.CHANELS} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__customerSegments}>
+          <List name={canvasList.CUSTOMER_SEGMENTS} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__cost}>
+          <List name={canvasList.COST_STRUCTURE} addCard={addElementHandler} />
+        </div>
+        <div className={style.canvas__revenue}>
+          <List name={canvasList.REVENUE_STREAMS} addCard={addElementHandler} />
+        </div>
+      </section>
+      {selectedList ? <Modal list={selectedList} modalClosed={handleModalClose} /> : null}
+    </Fragment>
   )
 }
 
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import style from './modal.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
 
-const Modal = () => {
+const Modal = ({ list, modalClosed }) => {
   return (
-    <div>
-      Modal
+    <div className={`${style.modal} shadow`}>
+      <div className={style.modal__controls}>
+        <p>{list}</p>
+        <span className={style.modal__close} onClick={modalClosed}>
+          <FontAwesomeIcon icon={faTimesCircle} size="lg" />
+        </span>
+      </div>
+      <div className={style.modal__input}>
+        <textarea placeholder="Card content"></textarea>
+      </div>
+      <button className="btn btn-primary">Add Card</button>
     </div>
   )
 }
