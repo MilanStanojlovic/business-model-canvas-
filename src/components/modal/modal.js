4ae5a7519d0f11bcf12f12417b809270ac756401
Milan <mistikone1@gmail.com>
2020-08-20 16:23:26 +0200

    modal functionality

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './modal.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
 
-const Modal = ({ list, modalClosed }) => {
+const Modal = ({ listName, modalClosed, addCard }) => {
+  const [value, setValue] = useState('');
+
   return (
     <div className={`${style.modal} shadow`}>
       <div className={style.modal__controls}>
-        <p>{list}</p>
+        <p>{listName}</p>
         <span className={style.modal__close} onClick={modalClosed}>
           <FontAwesomeIcon icon={faTimesCircle} size="lg" />
         </span>
       </div>
       <div className={style.modal__input}>
-        <textarea placeholder="Card content"></textarea>
+        <textarea placeholder="Card content" value={value} onChange={(event) => setValue(event.target.value)}></textarea>
       </div>
-      <button className="btn btn-primary">Add Card</button>
+      <button className="btn btn-primary" onClick={addCard.bind(this, value, listName)}>Add Card</button>
     </div>
   )
 }
