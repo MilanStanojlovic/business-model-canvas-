6301e19f5e9f1b45d3b08750e3b153733e0c5b14
Milan <mistikone1@gmail.com>
2020-08-22 13:26:05 +0200

    list sizes adjusted

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -4,14 +4,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 import CanvasCard from '../canvas-card/canvasCard';
+import { canvasList } from '../../enum/canvasList';
 
 const List = ({ name, showModal, list }) => {
   let content = null;
+  let listSize = style.list_md;
+
   if (list) {
     content = list.map(element => {
       return <CanvasCard key={element.id} id={element.id} content={element.content} />
     })
   }
+
+  if (name === canvasList.CUSTOMER_SEGMENTS || name === canvasList.VALUE_PROPOSITION || name === canvasList.KEY_PARTNERS) {
+    listSize = style.list_lg;
+  } else if (name === canvasList.REVENUE_STREAMS || name === canvasList.COST_STRUCTURE) {
+    listSize = style.list_sm;
+  }
+
 
   return (
     <section className={style.list}>
@@ -20,8 +30,8 @@ const List = ({ name, showModal, list }) => {
         <span className={style.list__info}><FontAwesomeIcon icon={faQuestionCircle} size="lg" /></span>
 
       </div>
-      <div className={style.list__content}>
-        { content }
+      <div className={`${style.list__content} ${listSize}`} >
+        {content}
       </div>
       <div className={style.list__add} onClick={showModal.bind(this, name)}><FontAwesomeIcon icon={faPlus} size="lg" /></div>
     </section>
