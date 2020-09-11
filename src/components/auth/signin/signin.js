4dc56af99a88f32943903a25d4b0c80c8e7f4987
Milan <mistikone1@gmail.com>
2020-09-11 18:59:46 +0200

    signin and signup firebase

--- src/components/auth/signin/signin.js
+++ src/components/auth/signin/signin.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../../firebase';
 
 const SignIn = ({ toggleAuth }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //TODO: 
+  //input valdidation
+  //error handling
+
+  const signInHandler = () => {
+    console.log(email, password);
+
+    auth.signInWithEmailAndPassword(email, password).then(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
   return (
     <>
       <div className="form">
         <h2 className="form__header">Welcome Back</h2>
         <p className="form__text">Sign in and create canvases!</p>
         <div>
-          <input type="email" placeholder="Email" />
+          <input type="email" placeholder="Email" value={email} onChange={event => { setEmail(event.target.value) }} />
         </div>
         <div>
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password" value={password} onChange={event => { setPassword(event.target.value) }} />
         </div>
-        <button className="btn btn-primary">Sign In</button>
+        <button className="btn btn-primary" onClick={signInHandler}>Sign In</button>
       </div>
       <p className="form__text">Don't have an account? <span className="blue-text icon" onClick={toggleAuth}>Sign Up.</span></p>
     </>
--- src/components/auth/signup/signup.js
+++ src/components/auth/signup/signup.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../../firebase';
 
 const SignUp = ({ toggleAuth }) => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //TODO:
+  //error handling 
+  //input validation
+
+  const registrationHandler = () => {
+    console.log(fullName, email, password);
+    // const auth = firebase.auth();
+
+
+    auth.createUserWithEmailAndPassword(email, password).then(response => {
+      console.log(response)
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
+
   return (
     <>
       <div className="form">
         <h2 className="form__header">Join Business Model</h2>
         <p className="form__text">Sign up and create your models!</p>
         <div>
-          <input type="text" placeholder="Full Name" />
+          <input type="text" placeholder="Full Name" value={fullName} onChange={(event) => { setFullName(event.target.value) }} />
         </div>
         <div>
-          <input type="email" placeholder="Email" />
+          <input type="email" placeholder="Email" value={email} onChange={(event) => { setEmail(event.target.value) }} />
         </div>
         <div>
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password" value={password} onChange={(event) => { setPassword(event.target.value) }} />
         </div>
-        <button className="btn btn-primary">Register</button>
+        <button className="btn btn-primary" onClick={registrationHandler}>Sign Up</button>
       </div>
       <p className="form__text">Already have an account? <span className="blue-text icon" onClick={toggleAuth}>Sign in.</span></p>
     </>
