d578b5028c12af0b1819fc316fa29beda39f80dd
Milan <mistikone1@gmail.com>
2020-09-18 12:10:55 +0200

    colored cards implementation

--- src/components/canvas-card/canvasCard.js
+++ src/components/canvas-card/canvasCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import style from './canvasCard.module.scss';
 
-const CanvasCard = ({ content, id }) => {
+const CanvasCard = ({ content, color, id }) => {
   return (
-    <div className={style.canvasCard}>
+    <div className={style.canvasCard} style={{ backgroundColor: `#${color}` }}>
       {content}
     </div>
   )
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import style from './modal.module.scss';
+
+import { colors } from '../../enum/colors';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
 
 const Modal = ({ listName, modalClosed, addCard }) => {
   const [value, setValue] = useState('');
+  const [selectedColor, setSelectedColor] = useState('ffadad');
+
+  const colorSelectHandler = (color) => {
+    if (color !== selectedColor) {
+      setSelectedColor(color);
+    }
+  }
 
   return (
     <div className={`${style.modal} shadow`}>
@@ -14,10 +24,20 @@ const Modal = ({ listName, modalClosed, addCard }) => {
           <FontAwesomeIcon icon={faTimesCircle} size="lg" />
         </span>
       </div>
-      <div className={style.modal__input}>
-        <textarea placeholder="Card content" value={value} onChange={(event) => setValue(event.target.value)}></textarea>
+      <div className={style.modal__colors}>
+        {colors.map((color) => {
+          return (
+            <div className={style.modal__color}
+              key={color}
+              onClick={colorSelectHandler.bind(this, color)}
+              style={{ backgroundColor: `#${color}` }}></div>
+          )
+        })}
       </div>
-      <button className="btn btn-primary" onClick={addCard.bind(this, value, listName)}>Add Card</button>
+      <div className={style.modal__input}>
+        <textarea placeholder="Card content" value={value} style={{ borderColor: `#${selectedColor}` }} onChange={(event) => setValue(event.target.value)}></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={addCard.bind(this, value, listName, selectedColor)}>Add Card</button>
     </div>
   )
 }
