51274c1227250cadb70119e0eca610f32fd39ffa
Milan <mistikone1@gmail.com>
2020-09-24 19:44:14 +0200

    save canvas modal

--- src/components/save-canvas/saveCanvas.js
+++ src/components/save-canvas/saveCanvas.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Backdrop from '../backdrop/backdrop';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+import { CanvasContext } from '../../context/CanvasContext';
+import { UiControlsContext } from '../../context/UIControlsContext';
 
 const SaveCanvas = () => {
-
+  const { setCanvasName, saveCanvas } = useContext(CanvasContext);
+  const { toggleSave } = useContext(UiControlsContext);
+  
   return (
     <Backdrop>
       <div className="modal">
         <div className="modal__controls">
           Save canvas
-          <FontAwesomeIcon icon={faTimesCircle} size="lg" className="modal__close icon" />
+          <FontAwesomeIcon icon={faTimesCircle} size="lg" className="modal__close icon" onClick={toggleSave} />
         </div>
         <form noValidate>
           <div className="modal__input">
-            <input type="text" placeholder="Canvas Name" name="canvas" />
+            <input type="text" placeholder="Canvas Name" name="canvas" onChange={(event) => { setCanvasName(event.target.value) }} />
           </div>
-          <button className="btn btn-primary">Save Canvas</button>
+          <button className="btn btn-primary" onClick={saveCanvas}>Save Canvas</button>
         </form>
       </div>
     </Backdrop>
