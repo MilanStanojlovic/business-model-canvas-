ed5983e87e720087ccebb97808047591e4ee5ca7
Milan <mistikone1@gmail.com>
2020-10-12 17:49:19 +0200

    add tooltip

--- src/components/canvas-item/canvasItem.js
+++ src/components/canvas-item/canvasItem.js
@@ -15,12 +15,18 @@ const CanvasItem = React.memo(({ canvasName, canvasId }) => {
       <div className={style.canvasItem__name}>{canvasName}</div>
 
       <div className={style.canvasItem__controls}>
-        <i className={`${style.canvasItem__icon} icon icon-circle`} onClick={renameCanvas.bind(this, canvasId)}>
-          <FontAwesomeIcon icon={faPencilAlt} size="sm" />
-        </i>
-        <i className={`${style.canvasItem__icon} icon icon-circle`} onClick={deleteCanvas.bind(this, canvasId)}>
-          <FontAwesomeIcon icon={faTrashAlt} size="sm" />
-        </i>
+        <span className="tooltip">
+          <i className={`${style.canvasItem__icon} icon icon-circle`} onClick={renameCanvas.bind(this, canvasId)}>
+            <FontAwesomeIcon icon={faPencilAlt} size="sm" />
+          </i>
+          <span className="tooltip__text">Rename</span>
+        </span>
+        <span className="tooltip">
+          <i className={`${style.canvasItem__icon} icon icon-circle`} onClick={deleteCanvas.bind(this, canvasId)}>
+            <FontAwesomeIcon icon={faTrashAlt} size="sm" />
+          </i>
+          <span className="tooltip__text">Delete</span>
+        </span>
       </div>
 
     </div>
